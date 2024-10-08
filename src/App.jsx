7d4142accca1b0113a14b40c743b8b3ce0fc0e52
venkadesh004 <venkadeshdeveloper@gmail.com>
2024-10-09 01:26:01 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CourseExitSurvey from "./pages/CourseExitSurvey";
+
+function App() {
+  return (
+    <div>
+      <CourseExitSurvey />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/API.js
@@ -0,0 +1,20 @@
+// const urlAddress = "https://tce-survey-backend.onrender.com/";
+const urlAddress = "http://localhost:5000/";
+
+// const routeAddresses = [
+//   "recruiterFeedback/addRecruiterFeedback",
+//   "parentsFeedback/addParentsFeedback",
+//   "courseExitSurvey/addCourseExitSurvey",
+//   "employerFeedback/addEmployerFeedback",
+//   "alumniFeedback/addAlumniFeedback",
+//   "graduateExitSurvey/addGraduateExitSurvey",
+//   "facultyFeedback/addFacultyFeedback",
+//   "peerFeedback/addPeerFeedback",
+// ];
+
+const routeAddresses = "courseExitSurvey/postData";
+
+const _urlAddress = urlAddress;
+export { _urlAddress as urlAddress };
+const _routeAddresses = routeAddresses;
+export { _routeAddresses as routeAddresses };
